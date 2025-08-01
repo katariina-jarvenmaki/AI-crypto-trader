68a5b59a65cc70b62642b034d2818f7454187f99
Katariina Jarvenmaki <katariina.jarvenmaki@gmail.com>
2025-08-01 09:48:04 +0300

    Configs updated

--- modules/equity_manager/config_equity_manager.py
+++ modules/equity_manager/config_equity_manager.py
@@ -6,9 +6,9 @@ LOG_FILE = "../AI-crypto-trader-logs/master_balance_log.jsonl"
 ALLOWED_TRADE_MARGIN_PERCENT = 25.0  # Should target to under 25.0
 
 # Stop trading on loss limit
-ALLOWED_EQUITY_MARGIN_PERCENT = 2.0 # Keep this under 25.0
-ALLOWED_PREV_EQUITY_MARGIN_PERCENT = 18.00 # Keep this under 25.0
-EQUITY_STOP_LOSS = 2.75 # Should be 0.5 or more over the ALLOWED_EQUITY_MARGIN_PERCENT
+ALLOWED_EQUITY_MARGIN_PERCENT = 15.0 # Keep this under 25.0
+ALLOWED_PREV_EQUITY_MARGIN_PERCENT = 25.00 # Keep this under 25.0
+EQUITY_STOP_LOSS = 15.5 # Should be 0.5 or more over the ALLOWED_EQUITY_MARGIN_PERCENT
 USE_PNL_MARGIN_IF_HIGHER = False
 
 # Keep as same as in UI
