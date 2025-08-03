4cf428ae985ae53813f2f741827a79d131d1b2ab
Katariina Jarvenmaki <katariina.jarvenmaki@gmail.com>
2025-08-04 00:09:59 +0300

    Clean the base

--- configs/btcc_config.py
+++
@@ -1,3 +0,0 @@
-# configs/btcc_config.py
-
-BTCC_API_URL = "https://api1.btloginc.com:9081"
--- global_state.py
+++
@@ -1,6 +0,0 @@
-# global_state.py
-"""
-Globaali tila moduuli.
-Tallentaa ja tarjoaa pääsyn 'positions_result'-dataan.
-"""
-POSITIONS_RESULT = {}
