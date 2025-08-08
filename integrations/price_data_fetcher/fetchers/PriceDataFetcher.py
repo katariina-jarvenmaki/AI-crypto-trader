2b6c1818942c8652563946e6320df45d409656cc
Katariina Jarvenmaki <katariina.jarvenmaki@gmail.com>
2025-08-08 21:29:11 +0300

    Updated fetcher on price_data_fetcher -module

--- integrations/price_data_fetcher/fetchers/PriceDataFetcher.py
+++ integrations/price_data_fetcher/fetchers/PriceDataFetcher.py
@@ -1,9 +1,9 @@
 # integrations/price_data_fetcher/fetchers/PriceDataFetcher
 
-# from integrations.price_data_fetcher.fetch_from_okx import fetch_from_okx
-# from integrations.price_data_fetcher.fetch_from_kucoin import fetch_from_kucoin
-# from integrations.price_data_fetcher.fetch_from_binance import fetch_from_binance
-# from integrations.price_data_fetcher.fetch_from_bybit import fetch_from_bybit
+from integrations.price_data_fetcher.fetchers.fetch_from_okx import fetch_from_okx
+from integrations.price_data_fetcher.fetchers.fetch_from_kucoin import fetch_from_kucoin
+from integrations.price_data_fetcher.fetchers.fetch_from_binance import fetch_from_binance
+from integrations.price_data_fetcher.fetchers.fetch_from_bybit import fetch_from_bybit
 
 class PriceDataFetcher:
 
@@ -12,35 +12,34 @@ class PriceDataFetcher:
         self.exchanges = order or CONFIG.get("exchange_priority", ["okx", "kucoin", "binance", "bybit"])
 
     def fetch(self):
-        print("Fetch")
-    #     for exchange in self.exchanges:
-    #         try:
-    #             method = getattr(self, f"fetch_from_{exchange}")
-    #             data = method()
-    #             if data:
-    #                 # Tarkista että data ei ole tyhjää
-    #                 price = data.get("lastPrice", 0)
-    #                 volume = data.get("volume", 0)
-    #                 high = data.get("highPrice", 0)
-    #                 low = data.get("lowPrice", 0)
+        for exchange in self.exchanges:
+            try:
+                method = getattr(self, f"fetch_from_{exchange}")
+                data = method()
+                if data:
 
-    #                 if any([price, volume, high, low]):
-    #                     data['source'] = exchange
-    #                     return data
+                    price = data.get("lastPrice", 0)
+                    volume = data.get("volume", 0)
+                    high = data.get("highPrice", 0)
+                    low = data.get("lowPrice", 0)
 
-    #         except Exception as e:
-    #             print(f"❌ Error fetching from {exchange}: {e}")
-    #             continue
-    #     return None
+                    if any([price, volume, high, low]):
+                        data['source'] = exchange
+                        return data
 
-    # def fetch_from_okx(self):
-    #     return fetch_from_okx(self.symbol)
+            except Exception as e:
+                print(f"❌ Error fetching from {exchange}: {e}")
+                continue
+        return None
 
-    # def fetch_from_kucoin(self):
-    #     return fetch_from_kucoin(self.symbol)
+    def fetch_from_okx(self):
+        return fetch_from_okx(self.symbol)
 
-    # def fetch_from_binance(self):
-    #     return fetch_from_binance(self.symbol)
+    def fetch_from_kucoin(self):
+        return fetch_from_kucoin(self.symbol)
 
-    # def fetch_from_bybit(self):
-    #     return fetch_from_bybit(self.symbol)
+    def fetch_from_binance(self):
+        return fetch_from_binance(self.symbol)
+
+    def fetch_from_bybit(self):
+        return fetch_from_bybit(self.symbol)
