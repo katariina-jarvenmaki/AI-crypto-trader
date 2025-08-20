3dd34668a5aea8358c564791182c3c5b8bdd6baf
Katariina Jarvenmaki <katariina.jarvenmaki@gmail.com>
2025-08-20 16:31:28 +0300

    Added testing code from trend reversal

--- modules/history_sentiment/trend_reversal.py
+++ modules/history_sentiment/trend_reversal.py
@@ -91,23 +91,3 @@ def detect_trend_shifts(
         return True, (timestamp, "rise", round(change, 5))
     else:
         return False, None
-
-if __name__ == "__main__":
-
-    bias_24h = {"bias": 0.3}
-    bias_1h = {"bias": -0.1}
-
-    log_path = "../AI-crypto-trader-logs/analysis_logs/history_sentiment_log.jsonl"
-    all_symbols = {'NMRUSDT', 'BIOUSDT', 'VANAUSDT', 'ICPUSDT', 'ETCUSDT', 'XRPUSDT', 'AVAXUSDT', 'NEARUSDT', 'DASHUSDT', 'ENSUSDT', 'XLMUSDT', 'PAXGUSDT', 'CAKEUSDT', 'MOVRUSDT', 'ETHUSDT', 'TREEUSDT', 'GPSUSDT', 'LPTUSDT', 'ZECUSDT', 'BCHUSDT', 'TRBUSDT', 'SOLUSDT', 'BANANAUSDT', 'AEROUSDT', 'PENDLEUSDT', 'CTSIUSDT', 'ARCUSDT', 'RADUSDT', 'OMNIUSDT', 'APTUSDT', 'AUCTIONUSDT', 'TRUMPUSDT', 'LTCUSDT', 'XAUTUSDT', 'XMRUSDT', 'TIAUSDT', 'OGNUSDT', 'YFIUSDT', 'COOKUSDT', 'EGLDUSDT', 'ALUUSDT', 'DEXEUSDT', 'GRASSUSDT', 'LINKUSDT', 'AVAILUSDT', 'XTZUSDT', 'TAOUSDT', 'HYPEUSDT', 'SUSDT', 'CTCUSDT', 'HBARUSDT', 'BNBUSDT', 'ILVUSDT', 'KSMUSDT', 'QNTUSDT', 'QTUMUSDT', 'UNIUSDT', 'REDUSDT', 'FBUSDT', 'UMAUSDT', 'SUIUSDT', 'BTCUSDT', 'RUNEUSDT', 'LDOUSDT', 'CVXUSDT', 'METISUSDT', 'MEMEUSDT', 'DIAUSDT', 'MORPHOUSDT', 'OGUSDT', 'ADAUSDT', 'INJUSDT', 'BEAMUSDT', 'DOTUSDT', 'RENDERUSDT'}
-    oldest_allowed = "2025-08-19T14:38:21.908610+03:00"
-    newest_allowed = "2025-08-20T14:38:21.908610+03:00"
-
-    entries = load_entries_in_time_range(
-        file_path=log_path,
-        symbols=all_symbols,
-        start_time=oldest_allowed,
-        end_time=newest_allowed
-    )
-
-    analysis = trend_reversal_analyzer(bias_24h, bias_1h, entries)
-    # print(analysis)
