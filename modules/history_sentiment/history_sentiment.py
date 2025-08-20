0f3e7b4ea427855b215bd17febd327f0720b405a
Katariina Jarvenmaki <katariina.jarvenmaki@gmail.com>
2025-08-20 19:37:01 +0300

    Rechecked History Sentiment main

--- modules/history_sentiment/history_sentiment.py
+++ modules/history_sentiment/history_sentiment.py
@@ -2,17 +2,17 @@
 # version 2.0, aug 2025
 
 # from itertools import chain
-# from dateutil.parser import isoparse
-# from datetime import datetime, timedelta
-# from utils.get_timestamp import get_timestamp
-# from utils.get_symbols_to_use import get_symbols_to_use
-# from utils.load_configs_and_logs import load_configs_and_logs
+from dateutil.parser import isoparse
+from datetime import datetime, timedelta
+from utils.get_timestamp import get_timestamp
+from utils.get_symbols_to_use import get_symbols_to_use
+from utils.load_configs_and_logs import load_configs_and_logs
 # from modules.history_sentiment.compute_bias import compute_bias
-# from utils.load_entries_in_time_range import load_entries_in_time_range
+from utils.load_entries_in_time_range import load_entries_in_time_range
 # from modules.save_and_validate.save_and_validate import save_and_validate
 # from modules.history_sentiment.trend_reversal import trend_reversal_analyzer
 
-# def sentiment_analyzer(all_symbols, history_config, history_entries, sentiment_entries, sentiment_log_path, sentiment_log_schema_path):
+# def sentiment_analyzer(all_symbols, history_config, history_entries, sentiment_config, sentiment_entries, sentiment_log_path, sentiment_log_schema_path):
 
 #     print(f"\n💹 Running Sentiment Analyzer...")
 
@@ -73,43 +73,43 @@ if __name__ == "__main__":
             "mid_folder": "analysis",
             "module_key": "history_sentiment",
             "extension": ".jsonl",
-            "return": ["full_log_path", "full_log_schema_path"]
+            "return": ["config", "full_log_path", "full_log_schema_path"]
         }
     ])
 
-#     symbol_log_path = configs_and_logs["symbol_full_log_path"]
-#     symbol_config = configs_and_logs["symbol_config"]
+    symbol_log_path = configs_and_logs["symbol_full_log_path"]
+    symbol_config = configs_and_logs["symbol_config"]
 
-#     result = get_symbols_to_use(symbol_config, symbol_log_path)
-#     all_symbols = result["all_symbols"]
+    result = get_symbols_to_use(symbol_config, symbol_log_path)
+    all_symbols = result["all_symbols"]
 
-#     history_config = configs_and_logs["history_config"]
-#     history_log_path = configs_and_logs.get("history_full_log_path")
+    history_config = configs_and_logs["history_config"]
+    history_log_path = configs_and_logs.get("history_full_log_path")
 
-#     now = isoparse(get_timestamp())
-#     oldest_allowed = (now - timedelta(hours=24)).isoformat()
-#     newest_allowed = now.isoformat()
+    now = isoparse(get_timestamp())
+    oldest_allowed = (now - timedelta(hours=24)).isoformat()
+    newest_allowed = now.isoformat()
 
-#     history_entries = load_entries_in_time_range(
-#         file_path=history_log_path,
-#         symbols=all_symbols,
-#         start_time=oldest_allowed,
-#         end_time=newest_allowed
-#     )
+    history_entries = load_entries_in_time_range(
+        file_path=history_log_path,
+        symbols=all_symbols,
+        start_time=oldest_allowed,
+        end_time=newest_allowed
+    )
 
-#     sentiment_log_path = configs_and_logs.get("sentiment_full_log_path")
-#     sentiment_log_schema_path = configs_and_logs.get("sentiment_full_log_schema_path")
-#     now = isoparse(get_timestamp())
-#     oldest_allowed = (now - timedelta(hours=24)).isoformat()
-#     newest_allowed = now.isoformat()
+    sentiment_config = configs_and_logs["sentiment_config"]
+    sentiment_log_path = configs_and_logs.get("sentiment_full_log_path")
+    sentiment_log_schema_path = configs_and_logs.get("sentiment_full_log_schema_path")
+    now = isoparse(get_timestamp())
+    oldest_allowed = (now - timedelta(hours=24)).isoformat()
+    newest_allowed = now.isoformat()
 
-#     sentiment_entries = load_entries_in_time_range(
-#         file_path=sentiment_log_path,
-#         symbols=all_symbols,
-#         start_time=oldest_allowed,
-#         end_time=newest_allowed
-#     )
+    sentiment_entries = load_entries_in_time_range(
+        file_path=sentiment_log_path,
+        start_time=oldest_allowed,
+        end_time=newest_allowed
+    )
 
-#     sentiment_data = sentiment_analyzer(all_symbols, history_config, history_entries, sentiment_entries, sentiment_log_path, sentiment_log_schema_path)
+    sentiment_data = sentiment_analyzer(all_symbols, history_config, history_entries, sentiment_config, sentiment_entries, sentiment_log_path, sentiment_log_schema_path)
     # print(f"\nSentiment_data: {sentiment_data}")
         
